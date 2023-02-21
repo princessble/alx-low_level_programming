301fc6744583798a0cdff450e41fe0f7aa070ab7
princessble <kwomofortuneb@gmail.com>
2023-02-21 11:04:33 -0800

    1 alphabet c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - print all alphabet, in lowercase
+ * followed by a new line
  */
-
-
-	void print_alphabet(void);
+void print_alphabet(void);
 	{
 		char letter;
 
@@ -16,4 +15,3 @@
 
 		_putchar('\n');
 	}
-
