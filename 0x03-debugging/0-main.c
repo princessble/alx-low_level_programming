65ce6b856249d5b6e02485fd75b592cdb97a3380
princessble <kwomofortuneb@gmail.com>
2023-02-22 05:43:57 -0800

    0 main c main h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
-int main(void)
+int main(void);
 {
 	int i;
 
