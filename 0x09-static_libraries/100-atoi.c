9c8a323fe20698a6bacaef33babc84754e4884e3
princessble <kwomofortuneb@gmail.com>
2023-03-10 19:04:48 -0800

    100-atoi.c

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int _atoi(char *s);
+{
+return (atoi(s));
+}
