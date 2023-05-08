8c3ed2a9260dd581d55fbd2a568ca8e898d7708d
Blessing Fortune <74838811+princessble@users.noreply.github.com>
2023-05-08 22:40:19 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * Create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * @txt_content: A pointer to a string to write to the file.
+ * @text_content: A pointer to a string to write to the file.
  * Return: If the function fails - -1.
  * Otherwise - 1.
  */
